515e9a460a8727383aa5542a43cebd9cf6f20b43
Tom De Vries <tdevries60@gmail.com>
2018-05-29 19:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(holday,name) {
+function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 
